427aa1b0369f6e290f767488894f69719fe87510
sisonke <culoe2@gmail.com>
2016-03-08 10:37:12 +0200

    changes

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,6 @@ exports.show = function (req, res, next) {
 	req.getConnection(function(err, connection){
 		if (err) return next(err);
 		connection.query('SELECT  products.id, products.product_name, categories.category_name FROM products INNER JOIN categories ON categories.id = products.category_id ORDER BY category_name', [], function(err, results) {
-//    connection.query
 		    if (err) return next(err);
     		res.render('productList', {
 					no_products : results.length === 0,
@@ -17,9 +16,8 @@ exports.showAdd = function(req, res){
 	req.getConnection(function(err, connection){
     connection.query('SELECT * FROM categories',function(err, results) {
   		if (err) return next(err);
-  	//	console.log(results);
   		res.render('add', {
-  							categories: results
+  				categories: results
   			});
 		   });
 	});
