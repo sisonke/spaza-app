6d6f9020b0607307efcbd569a3a0f3774b18011a
sisonke <culoe2@gmail.com>
2016-04-13 16:18:20 +0200

    modified searchbar using jquery and ajax ;-)

--- public/js/search.js
+++ public/js/search.js
@@ -2,17 +2,19 @@
 $(document).ready(function() {
   $("#search").keyup(function() {
     var value = $("#search").val();
+    console.log(value);
     $.get("/products/search/" + value, function(results) {
       $("#products").html(results);
 
-      $(document).ready(function() {
-        $("#search").keyup(function() {
-          var Value = $("#search").val();
+    });
+  });
+
+        $("#searchValues").keyup(function() {
+          var Value = $("#searchValues").val();
+          console.log(Value);
           $.get("/categories/search/" + Value, function(results) {
+            console.log(results);
             $("#categories").html(results);
           });
         });
       });
-    });
-  });
-});
